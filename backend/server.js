b2075f66d2cf23ca09d08b4653e17ab672e06491
Amulya Paritosh <amulya1171@gmail.com>
2024-10-29 17:47:20 +0530

    FEAT: created user register route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const userRoute = require("./routes/userRoute");
 
 const app = express()
 
@@ -12,6 +13,9 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(bodyParser.json())
+
+// Route Middleware
+app.use("/api/users", userRoute)
 
 // Routes
 app.get("/", (req, res) => {
@@ -27,4 +31,3 @@ mongoose
         })
     })
     .catch((err) => console.log(err));
-    

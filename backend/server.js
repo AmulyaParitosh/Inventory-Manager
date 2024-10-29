d6c7e48788abbd36772616fb44abdb8d6a082ca6
Amulya Paritosh <amulya1171@gmail.com>
2024-10-29 17:42:53 +0530

    FEAT: added user model and home route

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,17 @@ const app = express()
 
 const PORT = process.env.PORT || 5000;
 
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Home Page!");
+});
+
+// Connect to DB and start server
 mongoose
     .connect(process.env.MONGO_URI)
     .then(() => {

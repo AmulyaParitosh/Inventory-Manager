c2f96d8f17e271f89e1ad82928f477116a487444
Amulya Paritosh <amulya1171@gmail.com>
2024-11-13 23:54:01 +0530

    FEAT:Added update product and fixed image upload

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Product = require("../models/productModel");
-const { fileSizeFormatter } = require("../utils/fileUpload");
+const { fileSizeFormatter, handleUpload } = require("../utils/fileUpload");
 const cloudinary = require("cloudinary").v2;
 
 // Create Prouct
@@ -19,10 +19,9 @@ const createProduct = asyncHandler(async (req, res) => {
     // Save image to cloudinary
     let uploadedFile;
     try {
-      uploadedFile = await cloudinary.uploader.upload(req.file.path, {
-        folder: "Pinvent App",
-        resource_type: "image",
-      });
+      const b64 = Buffer.from(req.file.buffer).toString("base64");
+      let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+      uploadedFile = await handleUpload(dataURI);
     } catch (error) {
       res.status(500);
       throw new Error("Image could not be uploaded");
@@ -30,7 +29,7 @@ const createProduct = asyncHandler(async (req, res) => {
 
     fileData = {
       fileName: req.file.originalname,
-      filePath: uploadedFile.secure_url,
+      filePath: uploadedFile,
       fileType: req.file.mimetype,
       fileSize: fileSizeFormatter(req.file.size, 2),
     };
@@ -87,9 +86,69 @@ const deleteProduct = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Product deleted." });
 });
 
+const updateProduct = asyncHandler(async (req, res) => {
+  const { name, category, quantity, price, description } = req.body;
+  const { id } = req.params;
+
+  const product = await Product.findById(id);
+
+  // if product doesnt exist
+  if (!product) {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+  // Match product to its user
+  if (product.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  // Handle Image upload
+  let fileData = {};
+  if (req.file) {
+    // Save image to cloudinary
+    let uploadedFile;
+    try {
+      const b64 = Buffer.from(req.file.buffer).toString("base64");
+      let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+      uploadedFile = await handleUpload(dataURI);
+    } catch (error) {
+      res.status(500);
+      throw new Error("Image could not be uploaded");
+    }
+
+    fileData = {
+      fileName: req.file.originalname,
+      filePath: uploadedFile,
+      fileType: req.file.mimetype,
+      fileSize: fileSizeFormatter(req.file.size, 2),
+    };
+  }
+
+  // Update Product
+  const updatedProduct = await Product.findByIdAndUpdate(
+    { _id: id },
+    {
+      name,
+      category,
+      quantity,
+      price,
+      description,
+      image: Object.keys(fileData).length === 0 ? product.image : fileData,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  res.status(200).json(updatedProduct);
+});
+
 module.exports = {
   createProduct,
   getProducts,
   getProduct,
   deleteProduct,
+  updateProduct,
 };
--- backend/utils/fileUpload.js
+++ backend/utils/fileUpload.js
@@ -1,17 +1,19 @@
 const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
 
 // Define file storage
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads");
-  },
-  filename: function (req, file, cb) {
-    cb(
-      null,
-      new Date().toISOString().replace(/:/g, "-") + "-" + file.originalname
-    ); // 23/08/2022
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "uploads");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(
+//       null,
+//       new Date().toISOString().replace(/:/g, "-") + "-" + file.originalname
+//     ); // 23/08/2022
+//   },
+// });
+const storage = multer.memoryStorage();
 
 // Specify file format that can be saved
 function fileFilter(req, file, cb) {
@@ -41,4 +43,26 @@ const fileSizeFormatter = (bytes, decimal) => {
   );
 };
 
-module.exports = { upload, fileSizeFormatter };
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+async function handleUpload(file) {
+  try {
+    const res = await cloudinary.uploader.upload(file, {
+      resource_type: "auto",
+    });
+    if (res && res.secure_url) {
+      return res.secure_url; // Return only the secure URL
+    } else {
+      throw new Error("Upload failed: secure_url not available in response.");
+    }
+  } catch (error) {
+    console.error("Error uploading to Cloudinary:", error.message);
+    throw error; // Re-throw to be handled by caller
+  }
+}
+
+module.exports = { upload, fileSizeFormatter, handleUpload };
